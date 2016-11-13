5579c079f41b76aff0acfdd88f3def9abd76625d
Thomas RETY <thomas.rety@epitech.eu>
2016-11-13 23:38:34 +0100

    Creation d'objet

--- sql/create_database.py
+++ sql/create_database.py
@@ -1,13 +1,13 @@
 import sqlite3
 
-conn = sqlite3.connect('test_login.db')
+conn = sqlite3.connect('real_data.db')
 
 c = conn.cursor()
 
-#c.execute('''CREATE TABLE obj (date text, 
-#          description text,  text, quantty real, price real)''')
+c.execute('''CREATE TABLE obj (date text, name text, 
+          description text, place real, hidden boolean, owner real, id integer primary key autoincrement)''')
 
-#c.execute('''CREATE TABLE USER (user text, password text)''')
-c.execute("insert into USER values ('admin', 'admin')")
+c.execute('''CREATE TABLE USER (user text, password text, mail text)''')
+c.execute("insert into USER values ('admin', 'admin', 'admin\admin.com')")
 conn.commit()
 conn.close()
