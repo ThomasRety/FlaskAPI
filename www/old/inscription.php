c588a73ad6b5ec23479d7ec388149f0a36557bb3
mortifia <amela57290@gmail.com>
2016-11-18 16:16:36 +0100

    Add files via upload
    add 3

---
+++ www/old/inscription.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <!-- En-tête de la page -->
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css.css" />
+        <title>inawood home</title>
+</head>
