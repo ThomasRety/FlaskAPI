dadd275af812ed587325e595e252d32ecb91de1b
mortifia <amela57290@gmail.com>
2016-11-19 16:04:41 +0100

    Add files via upload
    MISE A JOUR 16H04 19/11/2016

---
+++ www/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <!-- En-tête de la page -->
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css.css" />
+            <meta name="viewport" content="width=device-width, user-scalable=no"/>
+
+        <title>inawood home</title>
+</head>
+<body>
+	<?php include("pages/header.html"); ?>
+	<section>
+		<?php include("pages/inscription.html"); ?>
+		<?php include("pages/login.html"); ?>
+		<br>
+	</section>
+</body>
