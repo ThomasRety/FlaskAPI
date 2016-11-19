15e3a19221edb42a17210c8261afbadc0aea86e0
mortifia <amela57290@gmail.com>
2016-11-19 13:47:07 +0100

    Add files via upload
    inscription test

---
+++ www/inscription.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <!-- En-tête de la page -->
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css.css" />
+            <meta name="viewport" content="width=device-width, user-scalable=no"/>
+
+        <title>inawood home</title>
+</head>
