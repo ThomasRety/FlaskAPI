7df06b1836c5e767fd6b051f9445819019093833
mortifia <amela57290@gmail.com>
2016-11-23 13:34:12 +0100

    Add files via upload

---
+++ www/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <!-- En-tête de la page -->
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css.css" />
+            <meta name="viewport" content="width=device-width, user-scalable=no"/>
+
+        <title>inawood home</title>
+</head>
+<body>
+	<?php include("pages/header.html"); ?>
+	<section id="section">
+		<article id="article_log"></article>
+		<article id="test_fonction"></article>
+	</section>
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script src="script/jquery_test.js" type="text/javascript"></script>
+	<script src="script/javascript_test.js" type="text/javascript"></script>
+</body>
---
+++ www/script/jquery_test.js
@@ -0,0 +1,14 @@
+$(function() {
+  // Ici, le DOM est entièrement défini
+  	$('#bouton_de_test').click(function() {
+  		$('#test_fonction').load('pages/actu.html');
+  	});
+
+  	$('#bouton_inscription').click(function() {
+  		$('#article_log').load('pages/inscription.html');
+  	});
+
+   	$('#bouton_login').click(function() {
+  		$('#article_log').load('pages/login.html');
+  	});
+});
