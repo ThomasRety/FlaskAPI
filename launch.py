50c99562ded49cc359432f7a9acc0a4374c4b349
Thomas RETY <thomas.rety@epitech.eu>
2016-11-24 17:10:41 +0100

    real_data.db

--- launch.py
+++ launch.py
@@ -4,11 +4,10 @@
 
 from flask import Flask, request, abort
 from flask_cors import CORS, cross_origin
-
-
 import os
 import sqlite3
 from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 cors = CORS(app, ressources={"/*":{"origins": "*"}})
 
@@ -39,6 +38,7 @@ def check_img(user, ide):
 @app.route('/modif_user/', methods=['POST'])
 def modif_login():
     if (request.method == 'POST'):
+        print('\n===============================================================\n')
         mail = request.form['adresse%mail']
         passw = request.form['password']
         username = request.form['username']
@@ -46,8 +46,10 @@ def modif_login():
         old_passw = request.form['old%password']
         old_username = request.form['old%username']
         if (old_mail == mail and passw == old_passw and old_username == username):
+            print("Aucune information n'as été modifiée")
             abort(403)
         if (len(mail) == 0 or len(passw) == 0 or len(username) == 0):
+            print("Un champs est manquant")
             abort(403)
         f = "select mail from user where user.user = '{}' and password = '{}'".format(old_username, old_passw)
         try:
@@ -56,6 +58,7 @@ def modif_login():
             abort(403)
         row = c.fetchall()
         if (len(row) == 0 or row[0][0] != old_mail):
+            print("L'adresse mail ne correspond pas au mot de passe")
             abort(403)
         f = "update user set user = '{}', mail = '{}', password = '{}' where mail = '{}' and password = '{}' and user = '{}'".format(username, mail, passw, old_mail, old_passw, old_username)
         try:
@@ -69,39 +72,54 @@ def modif_login():
 @app.route('/create_user/', methods=['POST'])
 def create_user():
     if (request.method == 'POST'):
+        print('\n===============================================================\n')
         print("Request recu", end=' ')
         mail, passw = request.form['adresse%mail'], request.form['password']
         user_name = request.form['username']
         print(mail, '|', passw, '|', user_name)
         if (len(mail) == 0 or len(passw) == 0 or len(user_name) == 0):
-            abort(403)
+            print("Un des champs est manquant")
+            return("Un des champs est manquant")
         f = '''select user from user where mail = '{}' '''.format(mail)
         try:
             c.execute(f)
         except sqlite3.OperationalError:
+            print("Erreur SQL")
+            print("REQUETE = '{}'".format(f))
             abort(403)
         row = c.fetchall()
         if len((row)) != 0:
             print("Le mail existe déjà")
-            return ("FAUX")
+            return ("Un compte existe déjà avec cette adresse mail")
         f = '''select user from user where user.user = '{}' '''.format(user_name)
         try:
             c.execute(f)
         except sqlite3.OperationalError:
+            print("ERREUR SQL")
+            print("REQUETE = '{}'".format(f))
             abort(403)        
         if len(c.fetchall()) != 0:
             print("ça existe déja")
-            return ("CA EXISTE DEJA")
+            return ("Ca existe déjà")
         else:
             #Il n'y as pas de même nom d'user
-            f = '''insert into user values ('{}', '{}', '{}')''' .format(user_name, passw, mail)
+            f = '''insert into user values ('{}', '{}', '{}', 'None')''' .format(user_name, passw, mail)
             try:
                 c.execute(f)
             except sqlite3.OperationalError:
+                print("Erreur SQL")
+                print("REQUETE = '{}'".format(f))
                 abort(403)
             conn.commit()
             print("fonctionné")
-            return ("Je dois vous prévenir que cela a fonctionné")
+            f = os.urandom(8)
+            a = "udpdate user set token = '{}' where mail = '{}'".format(f, mail)
+            print("L'user {} as été cée avec le token {}".format(mail, f))
+            try:
+                c.execute(a)
+            except sqlite3.OperationalError:
+                pass
+            return (f)
     else:
         abort(401)
 
@@ -133,26 +151,68 @@ def create_obj():
 @app.route('/login', methods=['POST'])
 def login():
     if (request.method == 'POST'):
+        print('\n===============================================================\n')
         mail = request.form['adresse%mail']
         print("mail = ", mail, end=' ')
-        passw = request.form['password']
-        print("& password = ", passw)
-        f = '''select password from user where mail = '{}' '''.format(mail)
+        try:
+            passw = request.form['password']
+            print("& password = ", passw)
+            t = 0
+        except:
+            token = request.form['token']
+            print("& token = ", token)
+            t = 1
+        if (t == 0):
+            f = '''select password from user where mail = '{}' '''.format(mail)
+        else:
+            f = "select token from user where mail = '{}'".format(mail)
         try:
             c.execute(f)
         except sqlite3.OperationalError:
+            print("REQUETE = '{}'".format(f))
             abort(403)
         try:
+            verif = 0
             result = c.fetchall()
-            if result[0][0] == passw:
-                print("Tu EST LOG")
+            if (t == 0):
+                if result[0][0] == passw:
+                    verif = 1
+                else:
+                    verif = 0
+            else:
+                if result[0][0] == token:
+                    verif = 1
+                else:
+                    verif = 0
+            if verif == 1:
                 f = os.urandom(8)
+                a = "udpdate user set token = '{}' where mail = '{}'".format(f, mail)
+                try:
+                    c.execute(a)
+                except sqlite3.OperationalError:
+                    pass
+                print("Tu est log avec le token {}".format(f))
                 return (f)
             else:
+                print("échec")
                 return ("echec")
         except IndexError:
+            print("échec 4")
             return ("echec")
     else:
         abort(401)
+
+
+@app.route('/db', methods=['GET','POST'])
+def get_the_db():
+    print('===============================================================\n')
+    f = "select * from user"
+    c.execute(f)
+    row = c.fetchall()
+    s = str(row)
+    a = "<p>" + s
+    s = a.replace('),', ')<br />')
+    s = s + "</p>"
+    return (s)
 
 
