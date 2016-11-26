817f291fc11071aec623613225ad2ea1a7ef05c2
Thomas RETY <thomas.rety@epitech.eu>
2016-11-26 13:45:41 +0100

    Hash des MTP + différents erreures

--- launch.py
+++ launch.py
@@ -8,12 +8,17 @@ from flask_cors import CORS, cross_origin
 import os
 import sqlite3
 from werkzeug.utils import secure_filename
+import hashlib
+
 
 app = Flask(__name__)
 cors = CORS(app, ressources={"/*":{"origins": "*"}})
 
 app.config.from_object(__name__)
 
+
+passwThomas = '5c92318242c515a45d23be7085ab2e494517a02db4a9d58ceae5e342a844d069a72253635a47ea5f210b662c21f98745261bd7b0a5bd5833c5332f6013f00af7'
+passwGuillaume = '9fe7a0518ec34d62fa601a605ccc3cf30a812146ea0e0a61e1d48f1de0cb433253b9ded9ebb83438354989452e42351046d55101304df82d242bce96eef8eadc'
 db = 'sql/real_data.db'
 doc = 'test'
 
@@ -38,10 +43,10 @@ def get_script():
 
 
 def do_admin(mail, password):
-    if (mail == "thomas.rety57@gmail.com" and password == "Tristan"):
+    if (mail == "thomas.rety57@gmail.com" and password == passwThomas):
         print("Thomas S'est log")
         return (True)
-    elif (mail == "amela57290@gmail.com" and password == "Mortifia"):
+    elif (mail == "amela57290@gmail.com" and password == passwGuillaume):
         print("Guillaume s'est log")
         return (True)
     else:
@@ -58,13 +63,15 @@ def modif_login():
         print('\n===============================================================\n')
         mail = request.form['adresse%mail']
         passw = request.form['password']
+        passw = (hashlib.sha512(passw.encode())).hexdigest()
         username = request.form['username']
         old_mail = request.form['old%mail']
         old_passw = request.form['old%password']
+        old_passw = (hashlib.sha512(old_passw.encode())).hexdigest()
         old_username = request.form['old%username']
         if (old_mail == mail and passw == old_passw and old_username == username):
             print("Aucune information n'as été modifiée")
-            abort(403)
+            return ("errnomodif")
         if (len(mail) == 0 or len(passw) == 0 or len(username) == 0):
             print("Un champs est manquant")
             abort(403)
@@ -76,7 +83,7 @@ def modif_login():
         row = c.fetchall()
         if (len(row) == 0 or row[0][0] != old_mail):
             print("L'adresse mail ne correspond pas au mot de passe")
-            abort(403)
+            return ("errauth")
         f = "update user set user = '{}', mail = '{}', password = '{}' where mail = '{}' and password = '{}' and user = '{}'".format(username, mail, passw, old_mail, old_passw, old_username)
         try:
             c.execute(f)
@@ -92,6 +99,7 @@ def create_user():
         print('\n===============================================================\n')
         print("Request recu", end=' ')
         mail, passw = request.form['adresse%mail'], request.form['password']
+        passw = (hashlib.sha512(passw.encode())).hexdigest()
         user_name = request.form['username']
         print(mail, '|', passw, '|', user_name)
         if (len(mail) == 0 or len(passw) == 0 or len(user_name) == 0):
@@ -107,7 +115,7 @@ def create_user():
         row = c.fetchall()
         if len((row)) != 0:
             print("Le mail existe déjà")
-            return ("Un compte existe déjà avec cette adresse mail")
+            return ("erremail")
         f = '''select user from user where user.user = '{}' '''.format(user_name)
         try:
             c.execute(f)
@@ -173,6 +181,7 @@ def login():
         print("mail = ", mail, end=' ')
         try:
             passw = request.form['password']
+            passw = (hashlib.sha512(passw.encode())).hexdigest()
             print("& password = ", passw)
             t = 0
         except:
@@ -230,6 +239,7 @@ def login():
 def get_the_db():
     print('===============================================================\n')
     mail, passw = request.form['adresse%mail'], request.form['password']
+    passw = (hashlib.sha512(passw.encode())).hexdigest()
     if (do_admin(mail, passw) == False):
         abort(403)
     f = "select * from user"
@@ -244,6 +254,7 @@ def get_the_db():
 @app.route('/get_html', methods=['POST'])
 def get_the_html():
     mail, passw = request.form['adresse%mail'], request.form['password']
+    passw = (hashlib.sha512(passw.encode())).hexdigest()
     if (do_admin(mail, passw) == False):
         abort(403)
     print("J'envoie le html")
@@ -253,6 +264,7 @@ def get_the_html():
 @app.route('/delete/<maile>', methods=['POST'])
 def delete_user(maile):
     mail, passw = request.form['adresse%mail'], request.form['password']
+    passw = (hashlib.sha512(passw.encode())).hexdigest()
     if (do_admin(mail, passw) == False):
         abort(403)
     f = "delete from user where mail = '{}'".format(maile)
@@ -265,8 +277,32 @@ def delete_user(maile):
     conn.commit()
     return ("Utilisateur % delete" % maile)
 
+@app.route('/sql/', methods=['POST'])
+def get_request():
+    mail, passw = request.form['adresse%mail'], request.form['password']
+    passw = (hashlib.sha512(passw.encode())).hexdigest()
+    if (do_admin(mail, passw) == False):
+        abort(403)
+    f = request.form['sql']
+    try:
+        c.execute(f)
+    except sqlite3.OperationalError as E:
+        return (E)
+    row = c.fetchall()
+    s = str(row)
+    return (row)
 
-
+@app.route('/delete/sql', methods=['POST'])
+def delete_the_sql():
+    mail, passw = request.form['adresse%mail'], request.form['password']
+    passw = (hashlib.sha512(passw.encode())).hexdigest()
+    if (do_admin(mail, passw) == False):
+        abort(403)
+    f = "delete from user"
+    c.execute(f)
+    conn.commit()
+    return ("done")
+    
 if __name__ == "__main__":
     get_script()
     app.run("0.0.0.0")
